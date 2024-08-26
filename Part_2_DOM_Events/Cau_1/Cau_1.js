da68a178381249f586a4839512d881f47583b228
Hieu_21078081 <buihieu26102003@gmail.com>
2024-08-26 15:38:57 +0700

    fix code Part 2 DOM_Event

--- Part_2_DOM_Events/Cau_1/Cau_1.js
+++ Part_2_DOM_Events/Cau_1/Cau_1.js
@@ -2,9 +2,26 @@ let score = 20;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 const displayMessage = function (message) {
+
     document.querySelector('.message').textContent = message;
 };
+document.querySelector(".check").addEventListener('click',()=>{
+  if(score!=0){
+    const inputValue =  document.querySelector('.guess').value
 
+    if(secretNumber< new Number(inputValue)){
+      displayMessage("Too high")
+    }else if(secretNumber> new Number(inputValue)){
+      displayMessage("Too low")
+    }else{
+      document.querySelector('.number').textContent = secretNumber+""
+      document.querySelector('.message').style.color = 'green'
+      displayMessage("Correct !!")
+    }
+  }
+  score--;
+  document.querySelector('.score').textContent = score;
+})
 document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
